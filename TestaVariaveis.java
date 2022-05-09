eda32b204e420235e7468b5c241ec2c2715da36b
Talita Alice <talitalionsmane@gmail.com>
2022-05-08 21:55:41 -0300

    alura curso

---
+++ TestaVariaveis.java
@@ -0,0 +1,22 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("Ola, Novo teste");
+		
+		int idade;
+		idade = 27;
+		
+		System.out.println(idade);
+		
+		idade = 20 + 7;
+		System.out.println(idade);
+		idade = (10 * 2)  + 7;
+		System.out.println(idade);
+		idade = (2 * 10) + 7;
+		
+		System.out.println(idade);
+		
+		System.out.println("a idade é: " + idade + ", Parabéns");
+	}
+}
